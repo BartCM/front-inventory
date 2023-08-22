95c3ed54a29a8a24eb87d84393c68db2001c3a57
bartcm <bartolocan@hotmail.com>
2023-08-22 17:00:25 +0200

    listar productos

---
+++ src/app/modules/shared/services/product.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+const base_url = "http://localhost:8083/api/v1";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(private http: HttpClient) { }
+
+  /**
+   * Muestra todos los productos
+   */
+  getProducts(){
+    const endpoint = `${base_url}/products`;
+    return this.http.get(endpoint);
+  }
+}
