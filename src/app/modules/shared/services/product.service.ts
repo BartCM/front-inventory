dec4edbcc30f55fcc8d07489277a11ad74cc32c8
bartcm <bartolocan@hotmail.com>
2023-09-22 18:00:02 +0200

    reportes y graficos

--- src/app/modules/shared/services/product.service.ts
+++ src/app/modules/shared/services/product.service.ts
@@ -54,4 +54,15 @@ export class ProductService {
     const endpoint = `${base_url}/products/filter/${name}`;
     return this.http.get(endpoint);
   }
+
+  /**
+   *
+   * export excel productos
+   */
+  exportProduct(){
+    const endpoint = `${base_url}/products/export/excel`;
+    return this.http.get(endpoint, {
+      responseType: 'blob'
+    });
+  }
 }
